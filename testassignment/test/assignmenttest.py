98debef9caa98fd3528e72e0403ce3cd7578ac3a
David Hernandez <davidhernandezruiz@gmail.com>
2018-07-25 01:31:34 +0200

    Adds function to the search class to get a list of words and return the json with the titles. Also improves the list unit tests to improve the speed by only downloading the file once.

--- testassignment/test/assignmenttest.py
+++ testassignment/test/assignmenttest.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 from unittest import mock
+from unittest.mock import MagicMock
 
 from testassignment.wordlist import WordList, filter_valid_words
 from testassignment.wordsearch import WordSearch
@@ -8,12 +9,14 @@ from testassignment.wordsearch import WordSearch
 class WordListTest(unittest.TestCase):
 
     def setUp(self):
+        self.wordList = WordList()
+
+    @classmethod
+    def setUpClass(cls):
         try:
             os.remove(WordList.WORDS_FILE)
         except:
             pass
-
-        self.wordList = WordList()
 
     def test_file_does_not_exist(self):
         self.assertFalse(self.wordList.file_exists())
@@ -31,6 +34,7 @@ class WordListTest(unittest.TestCase):
     def test_get_random_words(self):
         self.assertNotEqual(self.wordList.get_random_words(), self.wordList.get_random_words())
 
+# Mock for function get from requests
 def get_request_mock(*args, **keywargs):
     class MockResponse:
         def __init__(self, html):
@@ -52,6 +56,14 @@ class WordSearchTest(unittest.TestCase):
         titles = self.wordSearch.search('test')
 
         self.assertEqual(titles, ['first title', 'second title', 'third title'])
+
+    def test_json_output(self):
+        self.wordSearch.search = MagicMock(return_value=['title 1', 'title 2', 'title 3'])
+
+        titles = self.wordSearch.get_json(['test', 'cow'])
+
+        self.assertEqual(titles, '{"test": ["title 1", "title 2", "title 3"], "cow": ["title 1", "title 2", "title 3"]}')
+
 
 if __name__ == '__main__':
     unittest.main()
--- testassignment/wordsearch.py
+++ testassignment/wordsearch.py
@@ -1,6 +1,8 @@
+import json
 import lxml.html
 import requests
 
+from collections import OrderedDict
 from lxml.cssselect import CSSSelector
 
 class WordSearch():
@@ -13,9 +15,9 @@ class WordSearch():
         # Raise an exception if file could not be downloaded.
         r.raise_for_status()
 
-        return self.getTitles(r.text)
+        return self.get_titles(r.text)
 
-    def getTitles(self, html):
+    def get_titles(self, html):
         tree = lxml.html.fromstring(html)
 
         selector = CSSSelector('.result__title .result__a')
@@ -28,3 +30,12 @@ class WordSearch():
 
         return titles
 
+    def get_json(self, words):
+        results = OrderedDict()
+
+        for word in words:
+            titles = self.search(word)
+
+            results[word] = titles
+
+        return json.dumps(results)
